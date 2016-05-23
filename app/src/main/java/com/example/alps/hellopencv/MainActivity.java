34a3a09f888ab7ebb5cd8834c0b4977f0793d2f5
kys <scared22@naver.com>
2016-05-23 11:11:33 +0900

    opencv 라이브러리 3.0버전 사용 할 수 있도록 build.gradle에 추가

--- app/src/main/java/com/example/alps/hellopencv/MainActivity.java
+++ app/src/main/java/com/example/alps/hellopencv/MainActivity.java
@@ -1,6 +1,5 @@
 package com.example.alps.hellopencv;
 
-import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
@@ -11,7 +10,6 @@ import android.provider.MediaStore;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
